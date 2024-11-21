edeb856ab92dfc807f6afe75eb1bc29915e0d90c
JuaniV2002 <juani2002villa@icloud.com>
2024-11-21 11:45:28 -0300

    add final implementation of js

--- script.js
+++ script.js
@@ -14,7 +14,34 @@ function createGrid(size) {
     div.style.height = `${squareSize}px`;
 
     div.addEventListener('mouseenter', () => {
-      div.style.backgroundColor = 'black';
+      let h, s, l;
+
+      if (!div.dataset.hue) {
+        // First interaction: assign random color
+        h = Math.floor(Math.random() * 360); // Random hue
+        s = 100; // Full saturation
+        l = 100; // Start with lightness at 100%
+        div.dataset.hue = h;
+        div.dataset.saturation = s;
+        div.dataset.lightness = l;
+        div.dataset.darkenCount = 0;
+      } else {
+        // Subsequent interactions: darken color
+        h = parseInt(div.dataset.hue);
+        s = parseInt(div.dataset.saturation);
+        let darkenCount = parseInt(div.dataset.darkenCount);
+
+        if (darkenCount < 10) {
+          darkenCount += 1;
+          l = 100 - darkenCount * 10; // Decrease lightness by 10%
+          div.dataset.lightness = l;
+          div.dataset.darkenCount = darkenCount;
+        } else {
+          l = 0; // Fully darkened
+        }
+      }
+
+      div.style.backgroundColor = `hsl(${h}, ${s}%, ${l}%)`;
     });
 
     container.appendChild(div);
